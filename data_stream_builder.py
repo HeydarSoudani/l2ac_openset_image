47322ed599d7548062a6a4e8debb03e9d277c178
Heydar Soudani <heydar.soudani@gmail.com>
2021-10-12 00:31:41 +0330

    edit data_stream_builder file

--- data_stream_builder.py
+++ data_stream_builder.py
@@ -6,98 +6,75 @@ import gzip
 import numpy as np
 from pandas import read_csv
 import time
-
-int2text = {
-  0: 'tshirt',
-  1: 'trouser',
-  2: 'pullover',
-  3: 'dress',
-  4: 'coat',
-  5: 'sandal',
-  6: 'shirt',
-  7: 'sneaker',
-  8: 'bag',
-  9: 'ankle_boot',
-}
-textual_labels = ['tshirt', 'trouser', 'pullover', 'dress', 'coat', 'sandal', 'shirt', 'sneaker', 'bag', 'ankle_boot']
-
+import argparse
 
 def load_mnist(path, kind='train'):
-
   """Load MNIST data from `path`"""
-  labels_path = os.path.join(path,
-                              '%s-labels-idx1-ubyte.gz'
-                              % kind)
-  images_path = os.path.join(path,
-                              '%s-images-idx3-ubyte.gz'
-                              % kind)
+  labels_path = os.path.join(path,'%s-labels-idx1-ubyte.gz'% kind)
+  images_path = os.path.join(path,'%s-images-idx3-ubyte.gz'% kind)
 
   with gzip.open(labels_path, 'rb') as lbpath:
-      labels = np.frombuffer(lbpath.read(), dtype=np.uint8,
-                              offset=8)
-
+      labels = np.frombuffer(lbpath.read(), dtype=np.uint8,offset=8)
   with gzip.open(images_path, 'rb') as imgpath:
-      images = np.frombuffer(imgpath.read(), dtype=np.uint8,
-                              offset=16).reshape(len(labels), 784)
+      images = np.frombuffer(imgpath.read(), dtype=np.uint8, offset=16).reshape(len(labels), 784)
 
   return images, labels
 
-def main():
 
-  class_num = 10
-  seen_class_num = 5
-  unseen_class_num = 5
-  seen_samples_per_class = 1200
-  data_path = './data/'
-  train_file = 'm_train.csv'
-  stream_file = 'm_stream.csv'
-  # train_file = 'fm_train.csv'
-  # stream_file = 'fm_stream.csv'
-  # train_file = 'cifar10_train.csv'
-  # stream_file = 'cifar10_stream.csv'
+## == Params =====================
+parser = argparse.ArgumentParser()
+parser.add_argument('--class_num', type=int, default=10, help='')
+parser.add_argument('--seen_class_num', type=int, default=5, help='')
+parser.add_argument('--spc', type=int, default=1200, help='samples per class')
+parser.add_argument('--data_path', type=str, default='./data/', help='')
+parser.add_argument('--saved', type=str, default='./data/', help='')
+parser.add_argument('--dataset', type=str, default='mnist', help='') #[mnist, fmnist, cifar10]
+parser.add_argument('--train_file', type=str, default='m_train.csv', help='')
+parser.add_argument('--stream_file', type=str, default='m_stream.csv', help='')
+parser.add_argument('--seed', type=int, default=2, help='')
+args = parser.parse_args()
 
+## == Apply seed =================
+np.random.seed(args.seed)
+
+
+if __name__ == '__main__':
   ## ========================================
   # == Get MNIST dataset ====================
-  path = './build_dataset/data/mnist/'
-  train_data = read_csv(os.path.join(path, "train.csv"), sep=',', header=None).values
-  test_data = read_csv(os.path.join(path, "test.csv"), sep=',', header=None).values
+  if args.dataset == 'mnist':
+    train_data = read_csv(os.path.join(args.data_path, "train.csv"), sep=',', header=None).values
+    test_data = read_csv(os.path.join(args.data_path, "test.csv"), sep=',', header=None).values
 
-  X_train, y_train = train_data[:, 1:], train_data[:, 0]
-  X_test, y_test = test_data[:, 1:], test_data[:, 0]
-
-  # print(X_train.shape)
-  # print(y_train.shape)
-  # print(X_test.shape)
-  # print(y_test.shape)
-  # time.sleep(5)
+    X_train, y_train = train_data[:, 1:], train_data[:, 0]
+    X_test, y_test = test_data[:, 1:], test_data[:, 0]
   ## ========================================
   ## ========================================
 
   ## ========================================
   # == Get Fashion-MNIST dataset ============
-  # path = './build_dataset/data/fmnist/'
-  # X_train, y_train = load_mnist(path, kind='train') #(60000, 784), (60000,)
-  # X_test, y_test = load_mnist(path, kind='t10k')    #(10000, 784), (10000,)
+  if args.dataset == 'fmnist':
+    train_data = read_csv(os.path.join(args.data_path, "fashion-mnist_train.csv"), sep=',').values
+    test_data = read_csv(os.path.join(args.data_path, "fashion-mnist_test.csv"), sep=',').values
+    X_train, y_train = train_data[:, 1:], train_data[:, 0]
+    X_test, y_test = test_data[:, 1:], test_data[:, 0]
+    # X_train, y_train = load_mnist(path, kind='train') #(60000, 784), (60000,)
+    # X_test, y_test = load_mnist(path, kind='t10k')    #(10000, 784), (10000,)
   ## ========================================
   ## ========================================
 
   ## ========================================
   # == Get Cifar10 dataset ==================
-  # data_view = (3, 32, 32)
-  # path = './build_dataset/data/cifar10/'
-  # train_dataset = read_csv(os.path.join(path, 'cifar10_train.csv'), sep=',', header=None).values
-  # test_dataset = read_csv(os.path.join(path, 'cifar10_test.csv'), sep=',', header=None).values
-
-  # X_train = train_dataset[:, :-1]
-  # y_train = train_dataset[:, -1]
-  # X_test = test_dataset[:, :-1]
-  # y_test = test_dataset[:, -1]
+  if args.dataset == 'c10':
+    train_data = read_csv(os.path.join(args.data_path, 'cifar10_train.csv'), sep=',', header=None).values
+    test_data = read_csv(os.path.join(args.data_path, 'cifar10_test.csv'), sep=',', header=None).values
+    X_train, y_train = train_data[:, :-1], train_data[:, -1]
+    X_test, y_test = test_data[:, :-1], test_data[:, -1]
 
   ## ========================================
   ## ========================================
 
   ## ========================================
-  # == For normal training ==================
+  # == For batch training ===================
   # train_data = np.concatenate((X_train, y_train.reshape(-1, 1)), axis=1)   #(60000, 785)
   # test_data = np.concatenate((X_test, y_test.reshape(-1, 1)), axis=1)   #(10000, 785)
 
@@ -118,35 +95,20 @@ def main():
   data = np.concatenate((X_train, X_test), axis=0)  #(70000, 784)
   labels = np.concatenate((y_train, y_test), axis=0)#(70000,)
 
-  # convert to textual labels
-  # labels = np.array([int2text[int_label] for int_label in labels])
-
   # == split data by classes =================
-  # {label: list[],
-  #   ...}
-
   class_data = {}
   for class_label in set(labels):
-    class_data[class_label] = []
-  
+    class_data[class_label] = []  
   for idx, sample in enumerate(data):
     class_data[labels[idx]].append(sample)
 
-  # for class_label in set(labels):
-  #   print('{}: {}'.format(class_label, len(class_data[class_label])))
-  # time.sleep(4)
-
   # == Select seen & unseen classes ==========
-  seen_class = np.random.choice(class_num, seen_class_num, replace=False)
+  seen_class = np.random.choice(args.class_num, args.seen_class_num, replace=False)
   unseen_class = [x for x in list(set(labels)) if x not in seen_class]
   # seen_class = [0, 1, 2, 3, 4] 
   # unseen_class = [5, 6, 7, 8, 9]
-  # seen_class = np.random.choice(textual_labels, seen_class_num, replace=False)
-  # unseen_class = [x for x in list(set(labels)) if x not in seen_class]
-  
   print('seen: {}'.format(seen_class))
   print('unseen: {}'.format(unseen_class))
-  # time.sleep(2)
 
   # == Preparing train dataset and test seen data ===
   train_data = []
@@ -157,7 +119,7 @@ def main():
     del class_data[seen_class_item]
 
     np.random.shuffle(seen_data)
-    last_idx = seen_samples_per_class
+    last_idx = args.spc
     test_data_length = seen_data.shape[0] - last_idx
     train_part = seen_data[:last_idx]
     test_part = seen_data[last_idx:]
@@ -171,13 +133,13 @@ def main():
   train_data = np.array(train_data) #(6000, 785)
   np.random.shuffle(train_data)
   
-  pd.DataFrame(train_data).to_csv(os.path.join(data_path,train_file),
+  pd.DataFrame(train_data).to_csv(os.path.join(args.saved, args.train_file),
     header=None,
     index=None
   )
 
-  test_data_seen = np.array(test_data_seen) #(30000, 785)
   # == Preparing test(stream) dataset ================
+  test_data_seen = np.array(test_data_seen) #(30000, 785)
   all_temp_data = []
   add_class_point = 6000
   
@@ -189,7 +151,6 @@ def main():
 
     if len(unseen_class) != 0:
       rnd_uns_class = unseen_class[0]
-      # rnd_uns_class =  random.choice(unseen_class)
       unseen_class.remove(rnd_uns_class)
       
       selected_data = np.array(class_data[rnd_uns_class])
@@ -204,17 +165,15 @@ def main():
 
     if len(unseen_class) == 0:
       np.random.shuffle(test_data_seen)
-      # sections = episode_num - i - 1
-      # all_temp_data.extend(test_data_seen)
       all_temp_data = np.concatenate((all_temp_data, test_data_seen), axis=0)
       break
   
   test_data = np.stack(all_temp_data)
-  pd.DataFrame(test_data).to_csv(os.path.join(data_path,stream_file), header=None, index=None)
+  pd.DataFrame(test_data).to_csv(os.path.join(args.saved, args.stream_file),
+    header=None,
+    index=None
+  )
 
   # dataset = np.concatenate((train_data, test_data), axis=0)
   # file_path = './dataset/fashion-mnist_stream.csv'
   # pd.DataFrame(dataset).to_csv(file_path, header=None, index=None)
-
-if __name__ == '__main__':
-  main()
